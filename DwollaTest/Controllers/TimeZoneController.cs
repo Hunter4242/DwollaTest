06cac18a5b30005261ad4e95d989614ddb9d59a5
Hunter4242 <hunter4242@hotmail.com>
2024-05-30 10:09:05 -0400

    Time and Offset API implementation

---
+++ DwollaTest/Controllers/TimeZoneController.cs
@@ -0,0 +1,63 @@
+using DwollaTest.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+using System.Collections;
+
+
+namespace DwollaTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TimeZoneController : ControllerBase
+    {
+        private readonly ILogger<TimeZoneController> _logger;
+
+        public TimeZoneController(ILogger<TimeZoneController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Get the current UTC time, and optionally, the current time at the given UTC offset
+        /// </summary>
+        /// <param name="offset">A valid UTC offset in the format +##:## of -##:##</param>
+        /// <returns>A JSON response object with the currentTime and (if requested) the offsetTime</returns>
+        [HttpGet(Name = "time")]
+        public ActionResult<TimeZone> Get(string offset = "")
+        {
+            TimeZone time = new TimeZone();
+            time.currentTime = DateTime.UtcNow;
+            if(!string.IsNullOrEmpty(offset))
+            {
+                //check for valid offset
+                DateTime adjusted = ApplyOffset(offset, time.currentTime);
+                if(adjusted.Year == 1)
+                {
+                    return BadRequest("Invalid UTC Offset.");
+                }
+                else
+                {
+                    time.adjustedTime = adjusted;
+                }
+            }
+
+            return Ok(time);
+        }
+
+        private DateTime ApplyOffset(string offset, DateTime time)
+        {
+
+            DateTimeOffset dto;
+            if(DateTimeOffset.TryParse(time.ToShortDateString() + " " + time.ToShortTimeString() + " " + offset, null, out dto))
+            {
+                TimeSpan parsedOffset = dto.Offset;
+                return dto.DateTime.Add(parsedOffset);
+            }
+            else
+            {
+                return new DateTime(1,1,1);
+            }
+
+        }
+    }
+}
---
+++ DwollaTest/TimeZone.cs
@@ -0,0 +1,9 @@
+namespace DwollaTest
+{
+    public class TimeZone
+    {
+        public DateTime currentTime { get; set; }
+
+        public DateTime? adjustedTime { get; set; }
+    }
+}
